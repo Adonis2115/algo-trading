bea30e887aa9b1aeb17211e2e219c2480e68fbaf
Himanshu Pandey <himanshu21pandey@gmail.com>
2021-09-29 17:19:04 +0530

    Order Working

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express()
 
 const profile = require('./brokers/fyers/profile')
 const transaction = require('./brokers/fyers/transaction')
+const order = require('./brokers/fyers/order')
 
 app.use(profile)
 app.use(transaction)
+app.use(order)
 
 app.listen(5000, () => console.log(`Server Up and running at ${5000}`))
 
--- brokers/fyers/order.js
+++ brokers/fyers/order.js
@@ -3,12 +3,22 @@ const router = express.Router()
 const fyers = require('fyers-api-v2')
 const Credentials = require('../../../backtrader/brokers/fyers/credentials.json')
 
+router.use(express.json())
+
 fyers.setAppId(Credentials.appID)
 fyers.setRedirectUrl(Credentials.url)
 fyers.setAccessToken(Credentials.token)
 
-router.get('/profile', (req,res) => {
-    fyers.get_profile().then((response) => {
-        res.status(200).send(response.data)
+router.post('/submit', (req,res) => {
+    fyers.place_order(req.body).then((response) => {
+        res.status(200).send(response)
     })
 })
+
+router.post('/basket', (req,res) => {
+    fyers.place_multi_order(req.body).then((response) => {
+        res.status(200).send(response)
+    })
+})
+
+module.exports = router
